9efec3b72739d13a7a53f9bfe5fcd900b237e8f8
LIYANMUBARAK <115862601+LIYANMUBARAK@users.noreply.github.com>
2024-05-01 10:40:17 +0530

    feat:add bubble chart

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { NgApexchartsModule } from 'ng-apexcharts';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { PieChartComponent } from './components/dashboard/pie-chart/pie-chart.component';
+import { BubbleChartComponent } from './components/dashboard/bubble-chart/bubble-chart.component';
 
 
 @NgModule({
@@ -13,6 +14,7 @@ import { PieChartComponent } from './components/dashboard/pie-chart/pie-chart.co
     AppComponent,
     DashboardComponent,
     PieChartComponent,
+    BubbleChartComponent,
     
   ],
   imports: [
